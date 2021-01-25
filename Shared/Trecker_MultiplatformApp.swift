2d5f75890918344d0d4196bc0f759aaae7e809e9
Aidan Melville <amelville@lcs.on.ca>
2021-01-25 14:52:33 -0500

    Initial Commit

---
+++ Shared/Trecker_MultiplatformApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trecker_MultiplatformApp.swift
+//  Shared
+//
+//  Created by Melville, Aidan on 2021-01-25.
+//
+
+import SwiftUI
+
+@main
+struct Trecker_MultiplatformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
