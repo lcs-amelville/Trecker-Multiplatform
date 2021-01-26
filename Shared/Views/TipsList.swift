dc798129cce58f6cdd482d5147bf677e6ac20e56
Aidan Melville <amelville@lcs.on.ca>
2021-01-25 20:03:02 -0500

    Added Tab bar and tips and map view

---
+++ Shared/Views/TipsList.swift
@@ -0,0 +1,20 @@
+//
+//  TipsList.swift
+//  Trecker Multiplatform
+//
+//  Created by Melville, Aidan on 2021-01-25.
+//
+
+import SwiftUI
+
+struct TipsList: View {
+    var body: some View {
+        Text("Tips List View")
+    }
+}
+
+struct TipsList_Previews: PreviewProvider {
+    static var previews: some View {
+        TipsList()
+    }
+}
---
+++ Shared/Views/WorldMap.swift
@@ -0,0 +1,20 @@
+//
+//  WorldMap.swift
+//  Trecker Multiplatform
+//
+//  Created by Melville, Aidan on 2021-01-25.
+//
+
+import SwiftUI
+
+struct WorldMap: View {
+    var body: some View {
+        Text("World Map View")
+    }
+}
+
+struct WorldMap_Previews: PreviewProvider {
+    static var previews: some View {
+        WorldMap()
+    }
+}
